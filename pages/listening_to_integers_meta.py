5b8104e4a1fdfcf29d1c467c76c027040b8c1123
John Moon <moon.john.a@gmail.com>
2025-03-28 19:21:29 -0700

    Listening to integers post

---
+++ pages/listening_to_integers_meta.py
@@ -0,0 +1,11 @@
+
+from tocutil import TocEntry
+
+toc = TocEntry(
+	date='2025-03-28', 
+	title="Listening to the Integers", 
+	description="More surprises from the integers, with pictures and sound.",
+	logical_order=14, 
+	difficulty=2,
+	topics=["quantum computing","number theory"]
+)
