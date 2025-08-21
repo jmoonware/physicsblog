265f4890842ecaa3548dde8197387fadeb485627
John Moon <moon.john.a@gmail.com>
2025-08-21 06:01:55 -0700

    Bell intro

---
+++ pages/bell_introduction_meta.py
@@ -0,0 +1,11 @@
+
+from tocutil import TocEntry
+
+toc = TocEntry(
+	date='2025-08-17', 
+	title="A Gentle-ish Introduction to Quantum Measurement and Entanglement", 
+	description="All the fuss about quantum measurement, and how mutiple bits of the Universe seem to be connected in a way that seems impossible.",
+	logical_order=17, 
+	difficulty=1,
+	topics=["bells inequality"]
+)
